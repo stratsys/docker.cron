0b2bc9e051dfefadfa97ebc2b9228f25d1a0dc59
Johan Lindvall <johan.lindvall@stratsys.se>
2019-11-21 09:14:20 +0100

    Remove DOCKER_CRONFILES support.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-mkdir /cron && cd /cron && echo $DOCKER_CRONFILES | base64 -d | tar zxf -
 cat /cron/crontab | sed -e 's/$/\ > \/proc\/1\/fd\/1 2> \/proc\/1\/fd\/2/' | crontab -
 chmod +x /cron/*
 crontab -l && echo
